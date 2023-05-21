36b7db863fee074084ba82fe1bcbbf374c965647
Dark2801 <ripper2801@mail.ru>
2023-05-21 15:01:03 +0300

    Добавлен лейбл Счетчика, кнопка Счетчика и кнопка Сброса

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    // Создаем лейбл счетчика
+    private var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    // Этот метод обновляет значение лейбла счетчика
+    private func updateUI() {
+        counterLabel.text = "\(counter)"
     }
 
 
+    @IBAction func zoomButton(_ sender: Any) {
+        counter += 1
+        updateUI()
+    }
+    
+        @IBAction func resetButton(_ sender: Any) {
+        
+        counter = 0
+        updateUI()
+    }
 }
 
